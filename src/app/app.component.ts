fd987992be80b9fb29f2081e7243fa0e044b97eb
pengalaman-it <pengalaman.it@gmail.com>
2022-01-27 15:21:08 +0700

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
-import { Template } from '@angular/compiler/src/render3/r3_ast';
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import {FormControl} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
