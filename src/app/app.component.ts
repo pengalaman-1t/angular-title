3bececfa1a8c59c0a9871cffdbb6767c5492c088
pengalaman-it <pengalaman.it@gmail.com>
2022-01-27 15:15:05 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
 import { Component } from '@angular/core';
-
+import { Title } from '@angular/platform-browser';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
-  title = 'angular-title';
+  // title = 'angular-title';
+
+  public constructor(private titleService : Title){
+    this.titleService.setTitle("Button Ganti Title Gaes");
+  }
+public setTitle(newTitle: string){
+  this.titleService.setTitle(newTitle)
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+
+import {MaterialModule} from './shared/material.module';
+import { PopUpComponent } from './pop-up/pop-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, AppComponent, PopUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule, 
+    MaterialModule,
+    MatNativeDateModule,
+    HttpClientModule,
+    FormsModule, 
+    ReactiveFormsModule
+],
+  providers: [ Title ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
